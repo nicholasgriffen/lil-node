1b8d7959e268d9aa7219ad8a77397ae9b356bc55
nicholasgriffen <nicholas.s.griffen@gmail.com>
2019-04-02 11:04:04 -0600

    added some comments

--- test/server.test.js
+++ test/server.test.js
@@ -10,16 +10,19 @@ var PORT = process.env.PORT || 3030
     assert.equal(typeof server.start, 'function', 'expected start to be a function')
 
     console.log('server#start starts an http server at process.env.PORT or a default 3030')
-    
     server.start()
     
     http.get(`http://localhost:${PORT}`, function(res) {
         //wait for successful start test before stopping server
         console.log('server#stop is a function')    
         assert.equal(typeof server.stop, 'function', 'expected start to be a function')
+
         console.log('server#stop stops the server')
         server.stop()
+        
+        //make a request and expect it to error 
         http.get(`http://localhost:${PORT}`, function(res) {
+            //server is still running, stop did not work
             console.error('Expected server to stop')
         }).on('error', function(e) {
             //success, error should be ECONNREFUSED
