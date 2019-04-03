0a7e882eef79f6e6f0d07371a75f9a86ed3d9975
nicholasgriffen <nicholas.s.griffen@gmail.com>
2019-04-03 10:01:40 -0600

    test jwt

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-var testSuites = [ require('./server.test'), require('./router.test') ]
+var testSuites = [ require('./jwt.test'), require('./router.test'), require('./server.test') ]
 
 testSuites.forEach(function(suite) {
     suite.run()
--- test/router.test.js
+++ test/router.test.js
@@ -19,25 +19,25 @@ function mockReq(url, method) {
 }
 module.exports = {
     run: function() {
-        console.log('Running test suite for src/lib/routecr.js')
+        console.log('Running test suite for src/lib/router.js')
     
         console.log('router#handlers is a function')
         assert.equal(typeof router.handlers, 'function', 'expected handlers to be a function')
         
         console.log('router#handlers accepts a POST to /')
-        assert.doesNotThrow(function() { router.handlers(mockReq('/', 'POST'), res) })
+        router.handlers(mockReq('/', 'POST'), res)
         assert.notEqual(res.statusCode, 404, 'route should not 404')
 
         console.log('router#handlers accepts a GET to /validate')
-        assert.doesNotThrow(function() { router.handlers(mockReq('/validate', 'GET'), res) })
+        router.handlers(mockReq('/validate', 'GET'), res)
         assert.notEqual(res.statusCode, 404, 'route should not 404')
         
         console.log('router#handlers accepts a GET to /logout')
-        assert.doesNotThrow(function() { router.handlers(mockReq('/logout', 'GET'), res) })
+        router.handlers(mockReq('/logout', 'GET'), res)
         assert.notEqual(res.statusCode, 404, 'route should not 404')
 
         console.log('router#handlers sets a status code of 404 on unrecognized routes or methods')
-        assert.doesNotThrow(function() { router.handlers(mockReq('/', 'PUT'), res) })
+        router.handlers(mockReq('/', 'PUT'), res)
         assert.equal(res.statusCode, 404, 'unknown route or route + method should 404')
     }
 }
