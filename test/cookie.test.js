0363cc01a842c51eb8241feb8c44958216580eb9
nicholasgriffen <nicholas.s.griffen@gmail.com>
2019-04-03 10:39:00 -0600

    clarify test descriptors

--- test/cookie.test.js
+++ test/cookie.test.js
@@ -17,14 +17,14 @@ module.exports = {
         console.log('cookie#set is a function')
         assert.equal(typeof cookie.set, 'function', 'expected encode to be a function')
         
-        console.log('cookie#set takes a res, name, and value, and sets res.Set-Cookie to name=value; HttpOnly')
+        console.log('cookie#set takes a res, name, and value, and invokes res.setHeader with name and value')
         cookie.set(res, name, value)
         assert.equal(res['Set-Cookie'], setExpected, `expected res.Set-Cookie to be ${setExpected}`)
         
         console.log('cookie#expire is a function')
         assert.equal(typeof cookie.expire, 'function', 'expected encode to be a function')
         
-        console.log('cookie#expire takes a res, and name and sets res.Set-Cookie to name=\'\'; Expires=Mon Jan 01 1990 00:00:00 GMT-0700 (GMT-07:00)')
+        console.log('cookie#expire takes a res, and name and invokes res.setHeader with name, \'\'; Expires=Mon Jan 01 1990 00:00:00 GMT-0700 (GMT-07:00)')
         cookie.expire(res, name)
         assert.equal(res['Set-Cookie'], expireExpected, `expected res.Set-Cookie to be ${expireExpected}`)
     
